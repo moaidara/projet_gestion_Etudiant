e456eda89a4926d07b5e3e8e9c12b21304d3fff7
mo_aidara <mansouraidara43@gmail.com>
2024-11-17 14:53:48 +0100

    ajout de la fonction Ajouter_etudiant

--- python.py
+++ python.py
@@ -16,7 +16,7 @@ class Etudiant:
             
 #test
 etudiant2 = Etudiant('Mansour', 'Aidara', 'aidara300', 1234565, 21, 'Medina', 'l2')
-print(etudiant2.nom)
+# print(etudiant2.nom)
 
 #Creation d'un tableau
 tableau_Etudiant = [
@@ -25,7 +25,33 @@ tableau_Etudiant = [
 
 
 #Creation d'un dataframe
-# tableau_dataframe = pd.DataFrame(tableau_Etudiant, columns=['Nom', 'Prenom', 'nom_users', 'Matricule', 'Age', 'Adresse', 'niveau'])
+tableau_dataframe = pd.DataFrame(tableau_Etudiant, columns=['Nom', 'Prenom', 'nom_users', 'Matricule', 'Age', 'Adresse', 'niveau'])
 
 #Afficher le dataframe
 # print(tableau_dataframe['Matricule'])
+
+#Fonction Ajouter un Etudiant
+
+def Ajout_Etudiant():
+#importer les variables global dans la fonction pour eviter une confusion
+    global tableau_Etudiant
+    global tableau_dataframe
+#Recolter les information d'un etudiant
+    nom=input('Donnez le nom: ')
+    prenom=input('Donnez le prenom: ')
+    age=int(input('Donnez l age: '))
+    adresse=input('Donnez l adresse: ')
+    niveau=input('Donnez le niveau: ')
+    nom_users=input('Donnez le nom d utilisateur: ')
+    matricule=int(input('Donnez le matricule: '))
+#Creer une nouvelle instance de la classe Etudiant    
+    etudiant = Etudiant(nom, prenom, nom_users, matricule, age, adresse, niveau)
+#Ajouter dans le tableau
+    tableau_Etudiant.append([etudiant.nom, etudiant.prenom, etudiant.nom_users, etudiant.matricule, etudiant.age, etudiant.adresse, etudiant.niveau])
+#Mettre a jour le DataFrame   
+    tableau_dataframe = pd.DataFrame(tableau_Etudiant, columns=['Nom', 'Prenom', 'nom_users', 'Matricule', 'Age', 'Adresse', 'niveau'])
+
+
+#test
+# Ajout_Etudiant()
+# print(tableau_dataframe)
